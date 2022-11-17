4b822f48a0308d9177ff414d30f93a916d708658
Dinesh41 <dineshsaratha8@gmail.com>
2022-11-17 12:14:41 +0530

    First DB migration

---
+++ my books/Data/models/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace my_books.Data.models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+    }
+}
--- my books/Program.cs
+++ my books/Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using my_books.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string? dbConnectionString = builder.Configuration.GetSection("ConnectionStrings")["DefaultDBConnection"];
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(dbConnectionString));
 
 var app = builder.Build();
 
